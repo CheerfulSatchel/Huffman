f8c190310f2013ad07366d55f37efe1299d4ee9a
James Wu <therealjameswu@gmail.com>
2019-02-21 21:42:11 -0500

    Rename file

---
+++ huffman_enc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "huffman_node.h"
+
+int main() {
+
+  std::cout << "hayy" << std::endl;
+
+  HuffmanNode* nodeA = new HuffmanNode();
+  HuffmanNode* nodeB = new HuffmanNode();
+
+  nodeA->leftChild = nodeB;
+
+  std::cout << "It's ovah " << nodeA->leftChild->val << std::endl;
+
+  delete nodeB;
+  delete nodeA;
+
+  return 0;
+}
--- huffmanenc.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-
-  std::cout << "hayy" << std::endl;
-
-  return 0;
-}
