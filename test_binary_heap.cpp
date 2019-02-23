488452b3b1d614df4e6598abd778c0852dffca60
James Wu <therealjameswu@gmail.com>
2019-02-23 13:40:19 -0500

    Update testz

--- test_binary_heap.cpp
+++ test_binary_heap.cpp
@@ -4,15 +4,18 @@
 int main() {
   BinaryHeap heap;
 
-  HuffmanNode* nodeA = new HuffmanNode();
+  HuffmanNode* nodeA = new HuffmanNode(2, 'a');
+  HuffmanNode* nodeB = new HuffmanNode(1, 'b');
 
   heap.Insert(nodeA);
+  heap.Insert(nodeB);
 
   heap.Print();
 
   cout << "The min is: " << heap.FindMin()->freq << endl;
 
   delete nodeA;
+  delete nodeB;
   
   return 0;
 }
