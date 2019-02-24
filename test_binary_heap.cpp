9a65152f194251dae15b5a8038dd4e60b725cd7a
James Wu <therealjameswu@gmail.com>
2019-02-24 13:15:45 -0500

    Ensure heap is working properly...

--- test_binary_heap.cpp
+++ test_binary_heap.cpp
@@ -4,11 +4,15 @@
 int main() {
   BinaryHeap heap;
 
-  HuffmanNode* nodeA = new HuffmanNode(2, 'a');
+  HuffmanNode* nodeA = new HuffmanNode(2, 'd');
   HuffmanNode* nodeB = new HuffmanNode(1, 'b');
+  HuffmanNode* nodeC = new HuffmanNode(3, 'a');
+  HuffmanNode* nodeD = new HuffmanNode(1, 'c');
 
   heap.Insert(nodeA);
   heap.Insert(nodeB);
+  heap.Insert(nodeC);
+  heap.Insert(nodeD);
 
   heap.Print();
 
@@ -16,6 +20,8 @@ int main() {
 
   delete nodeA;
   delete nodeB;
+  delete nodeC;
+  delete nodeD;
   
   return 0;
 }
