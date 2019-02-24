0a1bb541af0cb95482fe6954b2b14d8a36e0c592
James Wu <therealjameswu@gmail.com>
2019-02-24 13:14:33 -0500

    Print in-order and add interface methods for encoding

--- huffman_tree.h
+++ huffman_tree.h
@@ -1,16 +1,34 @@
 #ifndef HUFFMAN_TREE_H
 #define HUFFMAN_TREE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unordered_map>
+#include <iostream>
+
 #include "binary_heap.h"
+
+using namespace std;
 
 class HuffmanTree {
  public:
+  // Default Constructor
   HuffmanTree();
   ~HuffmanTree();
 
+  // Methods
   void Insert(HuffmanNode* node);
   HuffmanNode* DeleteMin();
+  unordered_map<char, int> ReadInputFileToMap(FILE* fp);
+  void AddTableToHeap(unordered_map<char, int> frequencyTable);
+  void ConstructHuffmanTree();
+  void PrintTree(HuffmanNode* head);
 
+  // Getters
+  HuffmanNode* get_head_node();
+
+  
  private:
   BinaryHeap* heap_;  
 };
