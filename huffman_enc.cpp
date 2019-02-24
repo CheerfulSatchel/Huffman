630a76c2ac866a5f13223c748b6d4d5fb152d30c
James Wu <therealjameswu@gmail.com>
2019-02-23 22:36:19 -0500

    Read text files into unordered dictionary

--- huffman_enc.cpp
+++ huffman_enc.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
-#include "huffman_node.h"
+#include <unordered_map>
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "huffman_tree.h"
 
-int main() {
+int main(int argc, char **argv) {
 
-  std::cout << "hayy" << std::endl;
+  if (argc != 2) {
+    cout << "Please specify input file name" << endl;
+    exit(1);
+  }
+
+  FILE* fp = fopen(argv[1], "r");
+  if (!fp) {
+    cout << "File '" << argv[1] << "' not found!!" << endl;
+  }
+
+  unordered_map<char, int> frequencyTable;
+
+  char cItr;
+  while ((cItr = fgetc(fp)) != EOF) {
+    unordered_map<char, int>::const_iterator mapItr = frequencyTable.find(cItr);
+
+    // Initial insertion 
+    if (mapItr == frequencyTable.end()) {
+      cout << "Insertin new: " << cItr << endl;
+      frequencyTable.insert(pair<char, int>(cItr, 1));
+    } else { // Increment character frequency if already in the table
+      frequencyTable.at(cItr) += 1;
+    }
+
+    cout << "Yee: " << cItr << ", ";
+  }
+
+  unordered_map<char, int>::const_iterator itr;
+  
+  for (itr = frequencyTable.begin(); itr != frequencyTable.end(); ++itr) {
+    cout << itr->first << " : " << itr->second << endl;
+  }
+  
 
   HuffmanNode* nodeA = new HuffmanNode();
   HuffmanNode* nodeB = new HuffmanNode();
