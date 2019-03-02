3ad20b1088c2e67edcfaf65337ee1911c9740f32
James Wu <therealjameswu@gmail.com>
2019-03-02 14:00:28 -0500

    Abstract calls into HuffmanTree object

--- huffman_dec.cpp
+++ huffman_dec.cpp
@@ -12,8 +12,6 @@
 
 using namespace std;
 
-void RecreateTree(HuffmanNode* root, string character, string prefix);
-
 int main(int argc, char** argv) {
   if (argc != 2) {
     cout << "Missing input file name!" << endl;
@@ -23,6 +21,11 @@ int main(int argc, char** argv) {
   ifstream file(argv[1], ifstream::binary);
 
   HuffmanNode* root = new HuffmanNode(0, '-');
+
+  HuffmanTree tree;
+
+  tree.Insert(root);
+  
   HuffmanNode* itr = root;
   
   while (true) {
@@ -38,76 +41,30 @@ int main(int argc, char** argv) {
     cout << "character '" << character << "' has prefix code '"
 	 << prefix << "'" << endl;
 
-    RecreateTree(itr, character, prefix);
+    tree.ReconstructHuffmanTree(character, prefix);
     itr = root;
   }
-
-  HuffmanTree tree;
+  
   tree.Insert(root);
 
   tree.PrintTree(root);
+
+  string decoded = "";
   
-  stringstream sstm;
   while (true) {
     string bits;
     file >> bits;
 
     if (bits[0] == '-') break;
 
-    sstm << bits;
+    char c = tree.DecodeTree(bits);
+
+    decoded += c;
   }
 
-  string allbits = sstm.str();
-
-  cout << "All the bits: " << allbits << endl;
-
+  cout << decoded << endl;
+ 
   file.close();
   
   return 0;
-}
-
-void RecreateTree(HuffmanNode* root, string character, string prefix) {
-  HuffmanNode* currentNode = root;
-
-  char letter = character.c_str()[0];
-  const char* prefixes = prefix.c_str();
-
-  int i;
-  for (i=0; i<prefix.size(); ++i) {
-    if (prefixes[i] == '0') {
-      if (currentNode->leftChild) {
-	currentNode = currentNode->leftChild;
-      } else {
-        HuffmanNode* left;
-	// Internal '-' node
-	if (i+1 < prefix.size()) {
-	  left = new HuffmanNode(0, '-');
-	} else {
-	  // Child <letter> node
-	  left = new HuffmanNode(0, letter);
-	}
-
-	currentNode->leftChild = left;
-	currentNode = currentNode->leftChild;
-      }
-    } else if (prefixes[i] == '1') {
-      if (currentNode->rightChild) {
-	currentNode = currentNode->rightChild;
-      } else {
-	HuffmanNode* right;
-	// Internal '-' node
-	if (i+1 < prefix.size()) {
-	  right = new HuffmanNode(0, '-');
-	} else {
-	  // Child <letter> node
-	  right = new HuffmanNode(0, letter);
-	}
-
-	currentNode->rightChild = right;
-	currentNode = currentNode->rightChild;
-      }
-    } else {
-      throw "Unsupported character found!!!";
-    }
-  }
 }
